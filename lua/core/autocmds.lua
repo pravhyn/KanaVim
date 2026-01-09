950b59ab10e1cd912629c98b1766ca1568ea64a5
praveenscript <praveenkumarjkr@gmail.com>
2026-01-09 13:31:15 +0530

    added highlight on yank

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -17,14 +17,14 @@ end, {})
 
 -- highlights yank
 
-vim.api.nvim_create_autocmd("TextYankPost", {
-        callback = function()
-                vim.highlight.on_yank({
-                        higroup = "Visual",
-                        timeout = 500,
-                })
-        end,
-})
+-- vim.api.nvim_create_autocmd("TextYankPost", {
+--         callback = function()
+--                 vim.highlight.on_yank({
+--                         higroup = "Visual",
+--                         timeout = 500,
+--                 })
+--         end,
+-- })
 
 local function augroup(name)
         return vim.api.nvim_create_augroup("my_" .. name, { clear = true })
