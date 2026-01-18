d5f9eda8b2254906bf92606dc9a7dd161e149299
praveenscript <praveenkumarjkr@gmail.com>
2026-01-18 18:25:33 +0530

    some python snippets

--- snippets/python.lua
+++ snippets/python.lua
@@ -1,6 +1,9 @@
 local ls = require("luasnip")
 local s = ls.snippet
 local i = ls.insert_node
+local c = ls.choice_node
+
+local t = ls.text_node
 local fmt = require("luasnip.extras.fmt").fmt
 
 return {
@@ -18,4 +21,30 @@ async def {}(self{}):
                         }
                 )
         ),
+
+        s("ifni", {
+                t("if not isinstance("),
+                i(1, "obj"),
+                t(", "),
+                i(2, "Type"),
+                t("):"),
+                t({ "", "    " }),
+                i(3),
+        }),
+        s("ifis", {
+                t("if isinstance("),
+                i(1, "obj"),
+                t(", "),
+                i(2, "Type"),
+                t("):"),
+                t({ "", "    " }),
+                i(3),
+        }),
+
+        c(2, {
+                t("dict"),
+                t("list"),
+                t("str"),
+                t("int"),
+        }),
 }
