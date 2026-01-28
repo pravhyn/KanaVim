f966f51162dde552d46f544f02930b76edf270e7
praveenscript <praveenkumarjkr@gmail.com>
2026-01-28 09:23:59 +0530

    disabled suggested keymaps focus.nvim

--- lua/plugins/splits.lua
+++ lua/plugins/splits.lua
@@ -15,16 +15,16 @@ return {
         "nvim-focus/focus.nvim",
         version = false,
         config = function()
-                local focusmap = function(direction)
-                        vim.keymap.set("n", "<Leader>" .. direction, function()
-                                require("focus").split_command(direction)
-                        end, { desc = string.format("Create or move to split (%s)", direction) })
-                end
-
-                -- Use `<Leader>h` to split the screen to the left, same as command FocusSplitLeft etc
-                focusmap("h")
-                focusmap("j")
-                focusmap("k")
-                focusmap("l")
+                -- local focusmap = function(direction)
+                --         vim.keymap.set("n", "<Leader>" .. direction, function()
+                --                 require("focus").split_command(direction)
+                --         end, { desc = string.format("Create or move to split (%s)", direction) })
+                -- end
+                --
+                -- -- Use `<Leader>h` to split the screen to the left, same as command FocusSplitLeft etc
+                -- focusmap("h")
+                -- focusmap("j")
+                -- focusmap("k")
+                -- focusmap("l")
         end,
 }
