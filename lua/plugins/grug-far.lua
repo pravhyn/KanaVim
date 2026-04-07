f9d25bb7c8ef75c01301c67858adf49021152359
praveenscript <praveenkumarjkr@gmail.com>
2026-04-07 08:47:47 +0530

    fixed grugfarwithin

--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -3,12 +3,14 @@ return {
         config = function()
                 require("grug-far").setup({})
 
+                -- normal modes just opens it, visual will prefill the search string with visual selection
                 vim.keymap.set({ "n", "v" }, "<leader>sr", function()
                         require("grug-far").open()
                 end, { desc = "GrugFar: open search UI" })
 
+                --
                 vim.keymap.set("v", "<leader>sv", function()
-                        require("grug-far").open({ within = true })
+                        vim.cmd("GrugFarWithin")
                 end, { desc = "GrugFarWithin: search in visual selection" })
         end,
 }
